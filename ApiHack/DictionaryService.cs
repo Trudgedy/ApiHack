9496f8db81496d220abccfc6166df1834f65ee5d
Andrew <trudgedy@gmail.com>
2018-09-05 16:00:42 +0200

    Added files

---
+++ ApiHack/DictionaryService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+public class DictionaryService
+{
+
+    private String basePassword;
+
+	public DictionaryService()
+	{
+
+	}
+
+    public void CreateDictionary()
+    {
+        StreamWriter Stream;
+        
+        Stream = File.CreateText(@"E:\Andrew Projects\ApiHack\Dictionary\Dictionary.txt");
+        Stream.WriteLine(basePassword);
+
+
+        Stream.Close();
+        Console.WriteLine("Created File!");
+
+    }
+
+}
