7549861f8f7ae7e8c939eb37b77b895d52084f8b
Andrew <trudgedy@gmail.com>
2018-09-05 15:54:03 +0200

    Started

---
+++ ApiHack/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiHack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            DictionaryService dict = new DictionaryService();
+
+            dict.CreateDictionary();
+
+
+            var input = Console.Read();
+
+            //if (input == '')
+            //{
+
+            //}
+        }
+    }
+}
