294299165aa85163c81614972ccb23bd5a3d5882
Andrew Ross <Trudgedy@gmail.com>
2018-09-06 11:50:36 +0200

    Added serivces that dont work

--- ApiHack/DictionaryService.cs
+++ ApiHack/DictionaryService.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 public class DictionaryService
 {
 
-    private String basePassword;
+    private String basePassword = "Password";
 
 	public DictionaryService()
 	{
@@ -13,15 +14,25 @@ public class DictionaryService
 
     public void CreateDictionary()
     {
+        List<String> DictionaryOutput = null;
         StreamWriter Stream;
         
         Stream = File.CreateText(@"E:\Andrew Projects\ApiHack\Dictionary\Dictionary.txt");
         Stream.WriteLine(basePassword);
 
+        for (int i = 0; i < 20; i++)
+        {
+            DictionaryOutput.Add("Password");
+        }
 
+        Stream.WriteLine(DictionaryOutput);
         Stream.Close();
         Console.WriteLine("Created File!");
 
     }
+
+    LetterOption P = new LetterOption('P', new List<char> {'','',''});
+
+
 
 }
